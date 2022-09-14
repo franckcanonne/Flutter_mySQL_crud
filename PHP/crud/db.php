896a828ab3b69a5aecb454391bba115fd0cf67ea
Franck Canonne <39805659+franckcanonne@users.noreply.github.com>
2022-09-14 21:07:14 +0200

    Update db.php

--- PHP/crud/db.php
+++ PHP/crud/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_server="#######";
-$db_name="#######";
-$db_user="#######";
-$db_pass="#######";
+$db_server="#######"; // adresse de la base de données MySQL
+$db_name="#######"; // Nom de la base de données MySQL
+$db_user="#######"; // Utilisateur MySQL
+$db_pass="#######"; // Mot de passe MySQL
 
 $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
